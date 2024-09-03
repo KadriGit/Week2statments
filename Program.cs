0be1ce6c88b2bab2f492dedb63a3c319ca7d298f
KadriGit <kadri.viira@tktk.ee>
2024-09-03 16:09:49 +0300

    week 2

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Globalization;
+
+Console.WriteLine("Enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Enter your gender (f/m/n):");
+char userGender =Char.Parse(Console.ReadLine());
+
+String greeting = "";
+
+if (userGender =='f')
+{
+    greeting = "Ms.";
+}
+else if (userGender == 'm')
+{
+    greeting = "Mr.";
+}
+else
+{
+    greeting = "";
+}
+Console.WriteLine($"Hello, {greeting} {lastName}!");
+
+Console.WriteLine("Enter your year of birht");
+int userYB =Int32.Parse(Console.ReadLine());
+
+int currentYear=DateTime.Now.Year;
+
+
+int userAge = currentYear - userYB;
+
+
+
+
+if (userGender>=13)
+{
+    Console.WriteLine("Your are {usersYB} old enough to have account");
+
+}
+else if (userAge<13)
+{
+    Console.WriteLine("You are {usersYB} to young to have an account");
+
+}
